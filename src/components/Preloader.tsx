6ffb00f0851a75c358d51bbce9446daac60fe68a
andika <andikabrata10@gmail.com>
2026-06-14 10:10:26 +0700

    update

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -44,7 +44,7 @@ export function Preloader() {
               className="mb-8"
             >
               <h1 className="text-3xl font-bold tracking-tighter bg-gradient-to-r from-cyan-400 via-green-400 to-blue-400 bg-clip-text text-transparent">
-                Welcome to My Portfolio
+                Welcome to My Portofolio
               </h1>
             </motion.div>
 
